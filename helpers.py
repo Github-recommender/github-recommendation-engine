a664e4d32a64a118bcf7c9d66642a7b238b34723
Sharath Huddar <sharath.huddar@gmail.com>
2016-09-18 01:39:10 +0530

    Add 2nd degree follower's repos into db

--- helpers.py
+++ helpers.py
@@ -13,7 +13,40 @@ users = db.users
 users.ensure_index('username',unique=True)
 '''
 '''
+def insert_train_data(user):
+    kill = 0
+    KILL = 20
+    client = MongoClient()
+    db = client.github
+    repositories = db.repositories
+    for f in user.iter_followers():
+        if kill == KILL:
+            break
+        user2=user.user(f)
+        follower_followeres = (list(user2.iter_followers()))
+        for ff in follower_followeres:
+            if kill == KILL:
+                break
+            user3 = user.user(ff)
 
+            repo = list(user.iter_user_repos(user3))
+            for rep in repo:
+                kill = kill + 1
+                print 'repo:', kill
+                if kill == KILL:
+                    break
+                languages = list(rep.iter_languages())
+                summ=0
+                dictrepo = {
+                    rep.full_name:{}
+                }
+                for l in languages:
+                    summ=summ+int(l[1])
+                langs = {}
+                for l in languages:
+                    langs.update({l[0]:str(l[1]/float(summ))})
+                dictrepo[rep.full_name] = langs
+                repositories.insert_one(dictrepo)
 
 def insert_repos(user, repos):
     username = user.user().login
@@ -39,7 +72,10 @@ def insert_repos(user, repos):
         #print repo
         dictRepos.update(repo)
     users.update({'username':username},{'username':username,'repos':dictRepos} , True)
-    print type(dictRepos)
+    insert_train_data(user)
+
+
+
 def create_user_profile(user):
     client = MongoClient()
     db = client.github
